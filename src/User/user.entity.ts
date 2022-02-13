0e42698345a9141d34ddc219a80d753d406d8629
Ivan Galvagno <ivan.galvagno@gmail.com>
2022-02-13 17:29:07 -0300

    chore: Using class validator

--- src/User/user.entity.ts
+++ src/User/user.entity.ts
@@ -1,8 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString, Max, MaxLength, Min, MinLength } from "class-validator";
+
 export class User {
     id: number;
+
+    @IsNotEmpty({ message: 'nickname is required' })
+    @MinLength(4, {message: 'nickName need to be at least 4 characteres'})
+    @MaxLength(50, { message: 'nickName has to be less than 50 characteres'})
+    @IsString()
     nickName: string;
+
+    @IsEmail({},{  message: 'Invalid Email' })
     email: string;
+
+    @IsNotEmpty({ message: 'password is required' })
+    @MinLength(8, {message: 'password need to be at least 8 characteres'})
+    @MaxLength(50, { message: 'password has to be less than 50 characteres'})
     password: string;
+
+    @IsNotEmpty({  message: 'fullName is required' })
+    @MinLength(4, {message: 'fullName has to be more than 3 characteres'})
     fullName: string;
+
     JoinData: Date;
 }
